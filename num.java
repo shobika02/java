90ae1c790dab63c4a23ab4b3b40dd0953578dcc2
shobika02 <shobika.rece@ksrce.ac.in>
2016-07-28 17:12:05 +0530

    Add files via upload

---
+++ num.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class num
+{
+	public static void main(String[] arg)
+	{
+
+	Scanner in=new Scanner(System.in)
+	int a;
+	a=in.nextInt();
+	if(a%2==0)
+		System.out.println(n + "is even number");
+	else
+		System.out.println(n + "is odd number");
+
+	}
+
+}
