9cb6ccf290c84b978a5ae6278e1582c175542d71
shobika02 <shobika.rece@ksrce.ac.in>
2016-08-08 06:13:23 -0700

    Add files via upload

---
+++ NumberTest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class NumberTest
+{
+public static void main(String[],arg)
+{
+int a,i;
+Scanner in=new Scanner(System.in);
+a=in.nextInt();
+if(i>10&&i<50)
+{
+	if(a%2!=0)
+
+System.out.println(a+"is odd number");
+else
+
+System.out.println(a+"is not odd number");
+}
+}
+}
