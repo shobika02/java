89b9477e47c4de7c5417689f1f31e7e98d3fe8b2
shobika02 <shobika.rece@ksrce.ac.in>
2016-08-08 06:20:19 -0700

    Add files via upload

--- Number.java
+++ Number.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 class Number
 {
-           public static void main(String[] arg)
+public static void main(String[],arg)
 {
+int a,i;
+Scanner in=new Scanner(System.in);
+a=in.nextInt();
+if(i>10&&i<50)
+{
+	if(a%2==0)
 
-   Scanner in=new Scanner(System in);
-      int n;
-      System.out.println("enter the numbers:");
-     if(n>=0)
-      System.out.println(n + "is positive");
-           else
-      System.out.println( n + " is negative");
-           }
+System.out.println(a+"is even number");
+else
+
+System.out.println(a+"is not even number");
+}
+}
 }
