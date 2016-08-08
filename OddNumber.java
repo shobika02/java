1146b072641436252940eaf7bd3728ffa9559571
shobika02 <shobika.rece@ksrce.ac.in>
2016-08-08 06:17:47 -0700

    Add files via upload

---
+++ OddNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class OddNumber
+{
+public static void main(String[],arg)
+{
+int a,n;
+Scanner in=new Scanner(System.in);
+a=in.nextInt();
+if(n>10&&n<50)
+{
+	if(a%2!=0)
+
+System.out.println(a+"is odd number");
+else
+
+System.out.println(a+"is not odd number");
+}
+}
+}
