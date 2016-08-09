f544ce83c1b67257d28f9daf0f866cf86e7ac3f0
shobika02 <shobika.rece@ksrce.ac.in>
2016-08-09 18:10:57 +0530

    Add files via upload

---
+++ Revrse.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Revrse
+{
+	public static void main(String[] arg)
+	{
+	String s,t;
+	s=in.nextLine();
+	System.out.println("Enter the string to be reversed :");
+	t=new StringBuffer(s).reverse().toString();
+	System.out.println("reversed string :"+ t);	
+	}
+
+}
